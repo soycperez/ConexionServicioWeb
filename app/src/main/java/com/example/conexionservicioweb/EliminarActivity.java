5c7772b8a523921c542579d2f78b0f17dd0a100b
Cesar Perez <soycesar20k18@gmail.com>
2023-05-22 00:16:02 -0600

    Se añaden modificaciones

--- app/src/main/java/com/example/conexionservicioweb/EliminarActivity.java
+++ app/src/main/java/com/example/conexionservicioweb/EliminarActivity.java
@@ -3,12 +3,92 @@ package com.example.conexionservicioweb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class EliminarActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class EliminarActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener{
+
+    RequestQueue request;
+    JsonObjectRequest jsonObjectRequest;
+    Button btnBuscar;
+    Button btnEliminar;
+    EditText txtID;
+    EditText txtNom;
+    EditText txtCosto;
+    int estado = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        request = Volley.newRequestQueue(getApplicationContext());
+
         setContentView(R.layout.activity_eliminar);
+        btnBuscar = (Button) findViewById(R.id.button6);
+        txtID = (EditText) findViewById(R.id.editTextTextPersonName6);
+        txtNom = (EditText) findViewById(R.id.editTextTextPersonName7);
+        txtCosto = (EditText) findViewById(R.id.editTextNumberDecimal2);
+        btnEliminar= (Button) findViewById(R.id.button9);
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onclickSearch(view);
+            }
+        });
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onclikEliminar(view);
+            }
+        });
+    }
+
+    public void onclickSearch(View view){
+        estado = 0;
+        String line = "https://serviciosdigitalesplus.com/catalogo.php?tipo=1&id="+txtID.getText().toString()+"&r="+ Math.random();;
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, line, null, this,this);
+        request.add(jsonObjectRequest);
+    }
+
+    public void onclikEliminar(View view){
+        estado = 1;
+        String line = "https://serviciosdigitalesplus.com/catalogo.php?tipo=4&id="+txtID.getText().toString()+"&r="+ Math.random();
+        txtID.setText("");
+        txtNom.setText("");
+        txtCosto.setText("");
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, line, null, this, this);
+        request.add(jsonObjectRequest);
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        System.out.print("------------------------------------");
+        System.out.print("------------------------------------");
+        System.out.print(error.toString());
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        if(estado ==0){
+            JSONObject obj = response.optJSONObject("dato");
+            try{
+                txtNom.setText(obj.optString("nom"));
+                txtCosto.setText(obj.optString("costo"));
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/conexionservicioweb/MostrarActivity.java
+++ app/src/main/java/com/example/conexionservicioweb/MostrarActivity.java
@@ -3,30 +3,71 @@ package com.example.conexionservicioweb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
 import com.android.volley.VolleyError;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class MostrarActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener{
 
     RequestQueue request;
     JsonObjectRequest jsonObjectRequest;
+    EditText textArea;
+    Button btnListar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mostrar);
+
+        textArea = (EditText) findViewById(R.id.editTextTextMultiLine);
+        btnListar = (Button) findViewById(R.id.button10);
+        request = Volley.newRequestQueue(getApplicationContext());
+
+        btnListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onclickListar(view);
+            }
+        });
+    }
+
+    public void onclickListar(View view){
+        String line = "https://serviciosdigitalesplus.com/catalogo.php";
+        jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, line, null, this,this);
+        request.add(jsonObjectRequest);
+
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-
+        System.out.print("------------------------------------");
+        System.out.print("------------------------------------");
+        System.out.print(error.toString());
     }
 
     @Override
     public void onResponse(JSONObject response) {
-
+        JSONArray array = response.optJSONArray("datos");
+        try{
+            String aux = "";
+            for (int i = 0; i<array.length(); i++){
+                JSONObject obj = array.getJSONObject(i);
+                aux += "ID: " + obj.get("id") + "\n";
+                aux += "NOMBRE: " + obj.get("nom") + "\n";
+                aux += "COSTO: " + obj.get("costo") + "\n";
+                aux += "\n\n";
+            }
+            if(!aux.equals(""))
+                textArea.setText(aux);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
